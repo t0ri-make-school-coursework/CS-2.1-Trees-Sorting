9dd5e138be1221cada562f2ba799da6afde5bd3e
t0ri <victoriarosemurray@gmail.com>
2020-04-15 16:30:07 -0700

    add sorting_integer challenges

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -4,23 +4,56 @@
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
-    TODO: Running time: ??? Why and under what conditions?
+    Running time: O(range + n) because the amount of passes in loops depends
+    n and the maximum value that we use to init the histogram
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
-    # TODO: Loop over counts and append that many numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    if numbers:
+        # Find range of given numbers (minimum and maximum integer values)
+        maximum = max(numbers) # O(n)
+        minimum = min(numbers) # O(n)
+        
+        # Create list of counts with a slot for each number in input range
+        histogram = [0 for _ in range(maximum - minimum + 1)] # O(max)
+        
+        # Loop over given numbers and increment each number's count
+        for number in numbers: # O(n)
+            histogram[number - minimum] += 1 # O(1)
+        
+        # Loop over counts and append that many numbers into output list
+        pointer = 0
+        for index, item in enumerate(histogram):
+            for _ in range(0, item): # item is frequency
+                numbers[pointer] = index + minimum # O(n)
+                pointer += 1
+        
+    return numbers
 
 
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
+    Running time: O(n log n)
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
-    # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    if numbers:
+        # Find range of given numbers (minimum and maximum values)
+        maximum = max(numbers) # O(n)
+        minimum = min(numbers) # O(n)
+        
+        # Create list of buckets to store numbers in subranges of input range
+        buckets = [[] for _ in range(num_buckets)]
+        
+        # Loop over given numbers and place each item in appropriate bucket
+        for number in numbers:
+            bucket_index = (number * num_buckets) // (maximum + 1)
+            buckets[bucket_index].append(number)
+        
+        number_index = 0
+        # Loop over buckets and append each bucket's numbers into output list
+        for bucket in buckets:
+            # Sort each bucket using any sorting algorithm
+            bucket.sort()
+            for number in bucket:
+                numbers[number_index] = number
+                number_index += 1
+    
+    return numbers
--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -44,7 +44,7 @@ def merge_sort(items):
     Running time: O(n log n), work is divided by 2 each time
     Memory usage: O(n) to store items"""
     length = len(items)
-    if length is 1:
+    if length is 0 or length is 1:
         return items
     
     items1 = merge_sort(items[:length // 2])
@@ -55,16 +55,25 @@ def merge_sort(items):
 
 def partition(items, low, high):
     """Return index `p` after in-place partitioning given items in range
-    `[low...high]` by choosing a pivot (TODO: document your method here) from
+    `[low...high]` by choosing a pivot (low) from
     that range, moving pivot into index `p`, items less than pivot into range
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Choose a pivot any way and document your method in docstring above
-    # TODO: Loop through all items in range [low...high]
-    # TODO: Move items less than pivot into front of range [low...p-1]
-    # TODO: Move items greater than pivot into back of range [p+1...high]
-    # TODO: Move pivot item into final position [p] and return index p
+    # Choose a pivot any way and document your method in docstring above
+    pivot = low
+    # Loop through all items in range [low...high]
+    for i in range(low, high + 1):
+        # Move items less than pivot into front of range [low...p-1]
+        if items[i] < items[pivot] and i > pivot:
+            items.insert(low, items.pop(i))
+            pivot += 1
+        # Move items greater than pivot into back of range [p+1...high]
+        elif i < pivot:
+            items.insert(high, items.pop(i))
+            pivot -= 1
+    # Move pivot item into final position [p] and return index p
+    return pivot
 
 
 def quick_sort(items, low=None, high=None):
@@ -73,7 +82,15 @@ def quick_sort(items, low=None, high=None):
     TODO: Best case running time: ??? Why and under what conditions?
     TODO: Worst case running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if high and low range bounds have default values (not given)
-    # TODO: Check if list or range is so small it's already sorted (base case)
-    # TODO: Partition items in-place around a pivot and get index of pivot
-    # TODO: Sort each sublist range by recursively calling quick sort
+    # Check if high and low range bounds have default values (not given)
+    if low is None and high is None:
+        low = 0
+        high = len(items) - 1
+    # Check if list or range is so small it's already sorted (base case)
+    if high - low + 1 <= 1:
+        return items
+    # Partition items in-place around a pivot and get index of pivot
+    pivot_index = partition(items, low, high)
+    # Sort each sublist range by recursively calling quick sort
+    quick_sort(items, low, pivot_index - 1)
+    quick_sort(items, pivot_index + 1, high)
